b3c3221bc0a53a53b9be52d838a41f94209776f6
Caio <caiofilipee@gmail.com>
2021-08-11 14:03:42 -0300

    Criação da tabela

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Course } from './course';
 
 @Component({
@@ -16,17 +16,20 @@ export class CourseListComponent implements OnInit {
         name: 'Angular: Forms',
         imageUrl: '',
         price: 99.99,
-        code: 'XPS-8796'
+        code: 'XPS-8796',
         duration: 120,
-        rating: 4.5
+        rating: 4.5,
+        releaseDate: '10/08/2021'},
 
-        id: 2,
-        name: 'Angular: HTTP',
+      {  id: 2,
+        name: 'Angular: Forms2',
         imageUrl: '',
-        price: 45.99,
-        code: 'lkl-1094'
-        duration: 80,
-        rating: 4.0
+        price: 99.00,
+        code: 'XSS-7796',
+        duration: 189,
+        rating: 4.9,
+        releaseDate: '10/09/2021'
+
 
       }
     ]
